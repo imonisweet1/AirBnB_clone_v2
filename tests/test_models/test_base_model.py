4c096f0a806ade2fddbf29fa8758f473c302bcbc
Isaac Imonivwerha <isaacimonivwerha@gmail.com>
2024-04-24 05:02:43 -0700

    base model to tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -16,19 +16,6 @@ class test_basemodel(unittest.TestCase):
         super().__init__(*args, **kwargs)
         self.name = 'BaseModel'
         self.value = BaseModel
-<<<<<<< HEAD
-  #  """
-  #  A class to test pep8 on base_model file"""
-  #   def test_pycodestyle(self):
-  #      """
-  #      Test pep8 format
-  #      """
-  #      pycostyle = pycodestyle.StyleGuide(quiet=True)
-  #      result = pycostyle.check_files(['models/base_model.py'])
-  #      self.assertEqual(result.total_errors, 0,
-  #                       "Found code style errors (and warnings).")
-=======
->>>>>>> refs/remotes/origin/master
 
     def setUp(self):
         """ """
@@ -87,19 +74,11 @@ class test_basemodel(unittest.TestCase):
         with self.assertRaises(TypeError):
             new = self.value(**n)
 
-<<<<<<< HEAD
-     def test_kwargs_one(self):
-         """ """
-         n = {'Name': 'test'}
-         with self.assertRaises(KeyError):
-             new = self.value(**n)
-=======
     def test_kwargs_one(self):
         """ """
         n = {'Name': 'test'}
         with self.assertRaises(KeyError):
             new = self.value(**n)
->>>>>>> refs/remotes/origin/master
 
     def test_id(self):
         """ """
@@ -116,78 +95,6 @@ class test_basemodel(unittest.TestCase):
         new = self.value()
         #print(f"before save: {new.updated_at}")
         self.assertEqual(type(new.updated_at), datetime.datetime)
-<<<<<<< HEAD
-        n = new.to_dict()
-        new = BaseModel(**n)
-        self.assertFalse(new.created_at == new.updated_at)
-
-  #  def test_uuid(self):
-  #      """
-   #     Testin UUID
-        """
-        instance1 = BaseModel()
-        instance2 = BaseModel()
-        instance3 = BaseModel()
-        list_instances = [instance1, instance2,
-                          instance3]
-        for instance in list_instances:
-            ins_uuid = instance.id
-            with self.subTest(uuid=ins_uuid):
-                self.assertIs(type(ins_uuid), str)
-        self.assertNotEqual(instance1.id, instance2.id)
-        self.assertNotEqual(instance1.id, instance3.id)
-        self.assertNotEqual(instance2.id, instance3.id)
-
-    def test_str_method(self):
-        """Testing returns STR method"""
-        instance6 = BaseModel()
-        string_output = "[BaseModel] ({}) {}".format(instance6.id,
-                                                     instance6.__dict__)
-        self.assertEqual(string_output, str(instance6))
-
-
-class TestCodeFormat(unittest.TestCase):
-    """
-    A class to test pep8 on base_model file"""
-    def test_pycodestyle(self):
-        """
-        Test pep8 format
-        """
-        pycostyle = pycodestyle.StyleGuide(quiet=True)
-        result = pycostyle.check_files(['models/base_model.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-
-class Test_docstrings(unittest.TestCase):
-    """Test docstrings"""
-    @classmethod
-    def setup_class(self):
-        """
-        inspect.getmembers(object, [predicate])
-        Return all the members of an object in a list of (name, value)
-        pairs sorted by name
-        only members for which the predicate returns a true value are included
-        """
-        self.obj_members(BaseModel, inspect.isfunction)
-
-
-class TestBaseModel(unittest.TestCase):
-    """this will test the base model class x"""
-
-    @classmethod
-    def setUpClass(cls):
-        """setup for the test"""
-        cls.base = BaseModel()
-        cls.base.name = "Kev"
-        cls.base.num = 20
-
-    @classmethod
-    def teardown(cls):
-        """at the end of the test this will tear it down"""
-        del cls.base
-=======
->>>>>>> refs/remotes/origin/master
 
         import time
         time.sleep(1)
