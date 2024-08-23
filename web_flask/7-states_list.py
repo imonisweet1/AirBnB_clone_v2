dda720e4d83d73f9161e97b193426dbae6402b7a
Imonivwerha Isaac <isaacimonivwerha@gmail.com>
2024-08-23 22:24:14 +0100

    made corrections on web frame work

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,11 +5,18 @@ The application listens on 0.0.0.0, port 5000.
 Routes:
     /states_list: HTML page with a list of all State objects in DBStorage.
 """
+from flask import Flask, render_template
+from models.state import State
 from models import storage
-from flask import Flask
-from flask import render_template
+
 
 app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
@@ -22,11 +29,5 @@ def states_list():
     return render_template("7-states_list.html", states=states)
 
 
-@app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
-    storage.close()
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",port=5000)
